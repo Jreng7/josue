d840f7ef98776b9b65236e6ba4a5c1ec3fd3dc5d
Josue Ribeiro <jr.waydev@gmail.com>
2023-07-02 19:10:55 -0300

    feat: change light

--- main.js
+++ main.js
@@ -2,8 +2,8 @@ function toggleMode() {
   const html = document.documentElement
 
   if(html.classList.contains('light')) {
-    html.classList.remove('light')
-  } else {
     html.classList.add('light')
+  } else {
+    html.classList.remove('light')
   }
 }

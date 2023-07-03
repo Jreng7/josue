6252a552bf1048c9efe30e077a18bcb5f157af9c
waydev <jr.waydev@gmail.com>
2023-07-03 10:18:41 -0300

    feaT: main.js ok

--- main.js
+++ main.js
@@ -1,15 +1,9 @@
 function toggleMode() {
   const html = document.documentElement
 
-<<<<<<< HEAD
   if(!html.classList.contains('light')) {
     html.classList.remove('light')
   } else {
-=======
-  if(html.classList.contains('light')) {
->>>>>>> d840f7ef98776b9b65236e6ba4a5c1ec3fd3dc5d
     html.classList.add('light')
-  } else {
-    html.classList.remove('light')
   }
 }
